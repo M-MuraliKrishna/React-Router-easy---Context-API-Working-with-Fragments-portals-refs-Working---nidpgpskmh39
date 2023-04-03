f83d50765ed5b855dbcdb0dcbf4ec170c33a9b82
Murali Krishna M <murali24011999@gmail.com>
2023-04-03 22:49:44 +0530

    Updated

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import {Index }from './pages/Index'
+import {Home} from './pages/Home'
+import {NotFound} from './pages/NotFound'
 
 export const AppRoutes = () => {
     return (
-
+         <>           
+            <Routes>
+                <Route exact path = '/' element = {<Index/>}/>
+                <Route  exact path = '/home' element = {<Home/>}/>
+                <Route  path = '*' element = {<NotFound/>}/>                
+            </Routes>       
+        </>
     )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
-import '../styles/App.css';
-import { NavBar } from './NavBar';
+import {NavBar} from './NavBar'
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
 
   return (
     <BrowserRouter>
-      <div id="main">
-        <NavBar />
+      <div id="main"> 
+        <NavBar/>       
         <AppRoutes />
       </div>
     </BrowserRouter>
